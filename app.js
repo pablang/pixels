19727e407104c9d10e2d1536ef34a3ed3cf47f15
Pamela <pamablang@gmail.com>
2019-08-14 16:36:02 +1000

    Allow user to pick background from movie

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var setColorBtn = document.querySelector('.set-color-btn')
 var brushBox = document.querySelector('.brush-box')
 var squareDiv = document.querySelector('.square')
 var color = "blue"
+var movieBtn = document.querySelector('.movie-form button')
+var movieInput = document.querySelector(".movie-form input")
+var background = ''
+
+
 for (var i=0; i < 3000; i++){
   var pixel = document.createElement('div')
   pixel.classList.add('pixel')
@@ -21,10 +26,24 @@ var handleClick = function(event) {
 setColorBtn.addEventListener('click', handleClick)
 
 var changeColor = function(event){
-  console.log(event)
   event.target.style.backgroundColor = color
 }
 
 pixels.forEach(function(pixel) {
-  pixel.addEventListener('mouseover', changeColor)
+  pixel.addEventListener('click', changeColor)
 })
+
+var getMovie = function(event){
+  event.preventDefault()
+  var search = movieInput.value
+  var options = {
+    url: `http://www.omdbapi.com/?t=${search}&apikey=2f6435d9`
+  }
+  $.ajax(options).done(function(resp){
+    background = resp.Poster
+    squareDiv.style.background = url(background)
+  })
+
+}
+
+movieBtn.addEventListener('click', getMovie)
