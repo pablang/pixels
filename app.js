40d464c5acbe24e7a873120acbbef08b23c43ae6
Pamela <pamablang@gmail.com>
2019-08-14 15:49:30 +1000

    Allow pixel to change color

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 
+
 var setColorInput = document.querySelector('.set-color')
 var setColorBtn = document.querySelector('.set-color-btn')
 var brushBox = document.querySelector('.brush-box')
 var squareDiv = document.querySelector('.square')
-
-var handleClick = function(event) {
-  event.preventDefault()
-  brushBox.style.backgroundColor = setColorInput.value
-}
-
-setColorBtn.addEventListener('click', handleClick)
-
-for (var i=0; i <= 3000; i++){
+var color = "blue"
+for (var i=0; i < 3000; i++){
   var pixel = document.createElement('div')
   pixel.classList.add('pixel')
   squareDiv.appendChild(pixel)
 }
+var pixels = document.querySelectorAll('.pixel')
+
+var handleClick = function(event) {
+  event.preventDefault()
+  brushBox.style.backgroundColor = setColorInput.value
+  color = setColorInput.value
+}
+
+setColorBtn.addEventListener('click', handleClick)
+
+var changeColor = function(event){
+  event.target.style.backgroundColor = color
+}
+
+pixels.forEach(function(pixel) {
+  pixel.addEventListener('click', changeColor)
+})
